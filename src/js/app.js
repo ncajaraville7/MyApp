b87c50c77324ba00a50ef2f931178620e58fc343
Nicolas Cajaraville <nicolas.cajaraville7@gmail.com>
2021-11-18 15:41:51 -0300

    Primera entrega final

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,37 @@
-function bienvenida() {
-    let usuarioNombre = prompt('Ingrese su nombre');
+function seleccionarTarea() {
+    let consultaUsuario = prompt('Seleccione una tarea a realizar \n 1 -Depositar dinero a mi cuenta \n 2 -Realizar una transferencia \n 3-Realizar una cotización \n "ESC" - PARA FINALIZAR');
 
-    while(usuarioNombre === '') {
-        alert('¡Error! No ingresó ningún nombre');
-        usuarioNombre = prompt('Ingrese su nombre');
+    while(consultaUsuario != 'ESC' && consultaUsuario != 'Esc' && consultaUsuario != 'esc') {
+        switch(consultaUsuario) {
+            case '1':
+                let deposito = parseInt(prompt('Escriba la cantidad que desea depositar.'));
+                alert(`Deposito realizado con éxito. \n Monto depositado: $${deposito}`);
+                break;
+            case '2':
+                let transferencia = parseInt(prompt('Escriba la cantidad de dinero que desea transferir'));
+                if(deposito < transferencia) {
+                    alert('No tienes esa cantidad de dinero para transferir.')
+                } else {
+                    alert(`Transferencia realizada con éxito. \n Transfirió $${transferencia}`);
+                }
+                break;
+            case '3':
+                cotizar();
+                break;
+            default:
+                alert('Opción incorrecta');
+        }
+        consultaUsuario = prompt('Seleccione una tarea a realizar \n 1 -Depositar dinero a mi cuenta \n 2 -Realizar una transferencia \n 3-Realizar una cotización \n "ESC" - PARA FINALIZAR');
     }
-
-    alert(`¡Bienvenid@! ${usuarioNombre}`);
 }
 
+seleccionarTarea();
+
 function cotizar() {
-    let consultaUsuario = prompt('Seleccione cotización \n 1 - DE PESOS A DOLARES \n 2 - DE DOLAR A PESOS \n "ESC" - PARA FINALIZAR');
+    let consultaUsuario = prompt('Seleccione cotización \n 1 - DE PESOS A DOLARES \n 2 - DE DOLAR A PESOS \n "ESC" - PARA VOLVER ATRAS');
     const dolar = 199;
 
-    while(consultaUsuario != 'ESC') {
+    while(consultaUsuario != 'ESC' && consultaUsuario != 'Esc' && consultaUsuario != 'esc') {
         switch(consultaUsuario) {
             case '1':
                 let cotizaPesos = parseInt(prompt('Escriba la cantidad de pesos que quiere pasar a dolares'));
@@ -26,9 +44,7 @@ function cotizar() {
             default:
                 alert('Opción incorrecta');
         }
-        consultaUsuario = prompt('Seleccione cotización \n 1 - DE PESOS A DOLARES \n 2 - DE DOLAR A PESOS \n "ESC" - PARA FINALIZAR');
+        consultaUsuario = prompt('Seleccione cotización \n 1 - DE PESOS A DOLARES \n 2 - DE DOLAR A PESOS \n "ESC" - PARA VOLVER ATRAS');
     }
 }
 
-bienvenida();
-cotizar();
