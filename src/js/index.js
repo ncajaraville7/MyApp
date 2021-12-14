e969d554854257c0c14a697a8f2125aa9fac8ef7
Nicolas Cajaraville <nicolas.cajaraville7@gmail.com>
2021-12-14 18:50:33 -0300

    Se agrego jquery animaciones

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+const transfer = document.querySelector('.transfer');
+const loan = document.querySelector('.loan')
+
+$(transfer).animate({  
+    opacity: '0'  },
+    "slow",);
+
+    $(transfer).animate({  
+        opacity: '1'  },
+        "slow",);
+
+        $(loan).animate({  
+            opacity: '0'  },
+            "slow",);
+        
+            $(loan).animate({  
+                opacity: '1'  },
+                "slow",);
+
